6bb26a046fffdc564e033d0b469ff195b3d185ac
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-06 13:46:00 -0400

    Add files via upload

---
+++ VideoGameRead.java
@@ -0,0 +1,13 @@
+package Practice;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class VideoGameRead {
+
+	public static void main(String[] args) {
+		
+		}
+
+}
---
+++ VideoGameWriter.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.io.*;
+import java.util.*;
+
+public class VideoGameWriter {
+
+	public static void main(String[] args) {
+
+	}
+
+}
