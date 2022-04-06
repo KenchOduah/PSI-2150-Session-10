03b20da450382ebd45d54c637548c578a6dd59ac
KenchOduah <79539489+KenchOduah@users.noreply.github.com>
2022-04-06 13:53:54 -0400

    Add files via upload

--- VideoGameWriter.java
+++ VideoGameWriter.java
@@ -7,6 +7,20 @@ public class VideoGameWriter {
 
 	public static void main(String[] args) {
 
+		
+		//1. Create some data for the file
+		
+		//2. Create the print writer and file objects names
+		
+		//3. open the file to being writing in it(dont forget try/catch blocks)
+		//Start by giving the file a name using String
+		//Initialize the file objects
+		
+		//4. print the games using an enhanced for loop (dont forget try/catch blocks)
+		// Example of enhanced for loop: for(something s : something else)
+		// hint for printing games: ___________.println (array list/linked list name.____________) 
+		
+		//5. use finally to close the print writer object
 	}
 
 }
